02dd0f26c89ca5b8a02cf9a7ccbbfd81a8ce069a
Ayush CSPL <ayushgupta.cspl@gmail.com>
2025-12-23 17:26:25 +0530

    ready for deployment

--- queue/pdfQueue.js
+++ queue/pdfQueue.js
@@ -1,12 +1,9 @@
+import "dotenv/config";
 import { Queue } from "bullmq";
 import IORedis from "ioredis";
 
-export const connection = new IORedis({
-  host: "127.0.0.1",
-  port: 6379,
-  maxRetriesPerRequest: null
+export const connection = new IORedis(process.env.REDIS_URL, {
+  maxRetriesPerRequest: null,
 });
 
-export const pdfQueue = new Queue("pdf-jobs", {
-  connection
-});
+export const pdfQueue = new Queue("pdf-jobs", { connection });
--- server.js
+++ server.js
@@ -1,27 +1,20 @@
+import "dotenv/config";        // MUST be first
+
 import express from "express";
 import pdfRoutes from "./routes/pdf.js";
-import { validatePdfRequest } from "./middleware/validateRequest.js";
 import cors from "cors";
 import internalRoutes from "./routes/internal.js";
 
-
-
 const app = express();
 
-app.use(cors({
-  origin: [
-    "http://localhost:5500",
-    "http://127.0.0.1:5500"
-  ]
-}));
-
+app.use(cors());
 app.use(express.json({ limit: "1mb" }));
 
-// IMPORTANT: mount router at /pdf
 app.use("/pdf", pdfRoutes);
 app.use("/internal", internalRoutes);
 
+const PORT = process.env.PORT || 4000;
 
-app.listen(4000, () => {
-  console.log("📄 PDF Service running on http://localhost:4000");
+app.listen(PORT, () => {
+  console.log(`📄 PDF Service running on http://localhost:${PORT}`);
 });
