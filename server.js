a44454402bef84bdddb7130665561e73dbd23362
Ayush CSPL <ayushgupta.cspl@gmail.com>
2026-03-31 13:02:33 +0530

    added cors to accespt all origin

--- server.js
+++ server.js
@@ -7,7 +7,11 @@ import internalRoutes from "./routes/internal.js";
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: "*",
+  methods: ["GET","POST"],
+  allowedHeaders: ["Content-Type"]
+}))
 app.use(express.json({ limit: "1mb" }));
 
 app.use("/pdf", pdfRoutes);
